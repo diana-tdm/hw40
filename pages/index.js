8aae76d592de3089d9f250d66ad5e77a1d99f623
Tolkacheva Diana <sun_ray@mail.ru>
2022-07-05 15:55:07 +0500

    Исправила ошибки сборки

--- pages/index.js
+++ pages/index.js
@@ -56,11 +56,11 @@ export default function App() {
     <div className="wrap">
       <Head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Homework JSON, Ajax</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;900&display=swap"
           rel="stylesheet"
